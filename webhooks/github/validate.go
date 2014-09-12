14c9859b4e85d58b044e1856260dba3ab4629ad1
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-09-12 03:12:48 -0700

    Add GitHub webhook validation package

---
+++ webhooks/github/validate.go
@@ -0,0 +1,27 @@
+package github
+
+import (
+	"fmt"
+	"crypto/sha1"
+	"crypto/hmac"
+)
+
+// IsValidPayload checks if the github payload's hash fits with
+// the hash computed by GitHub sent as a header
+func IsValidPayload(headerHash string, payload []byte) {
+	hash := HashPayload(payload)
+	return hmac.Equal(
+		[]byte(hash),
+		[]byte(headerHash),
+	)
+}
+
+
+// HashPayload computes the hash of payload's body according to the webhook's secret token
+// see https://developer.github.com/webhooks/securing/#validating-payloads-from-github
+// returning the hash as a hexadecimal string
+func HashPayload(secret string, playloadBody []byte) string {
+	hm := hmac.New(sha1.New, []byte(secret))
+	sum := hm.Sum(playloadBody)
+	return fmt.Sprintf("%x", sum)
+}
